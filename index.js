45d1ac514bbf1678d1b96a3855938a47f882ab8a
nswbmw1992 <nswbmw1992@gmail.com>
2015-04-03 00:19:28 +0800

    fallback codes

--- index.js
+++ index.js
@@ -71,50 +71,48 @@ function scheme(conf, options) {
       Object.keys(flat_conf_request).forEach(function (key) {
         if (flat_ctx_request[key] === undefined) {
           debug('%s %s <- %s', ctx.method, ctx.path, key + ' : Not exist!');
-          if (options.debug) return ctx.throw(400, key + ' : Not exist!');
+          if (options.debug) ctx.throw(400, key + ' : Not exist!');
         }
         if ('function' === typeof flat_conf_request[key]) {
           if(!flat_conf_request[key].call(ctx, flat_ctx_request[key])) {
             debug('%s %s <- %s : %s ✖ [Function: %s]', ctx.method, ctx.path, key, flat_ctx_request[key], (flat_conf_request[key].name || 'function'));
-            if (options.debug) return ctx.throw(400, key + ' : ' + flat_ctx_request[key] + ' ✖ [Function: ' + (flat_conf_request[key].name || 'function') + ']');
+            if (options.debug) ctx.throw(400, key + ' : ' + flat_ctx_request[key] + ' ✖ [Function: ' + (flat_conf_request[key].name || 'function') + ']');
           }
         } else {
           if (!RegExp(flat_conf_request[key]).test(flat_ctx_request[key])) {
             debug('%s %s <- %s : %s ✖ %s', ctx.method, ctx.path, key, flat_ctx_request[key], flat_conf_request[key]);
-            if (options.debug) return ctx.throw(400, key + ' : ' + flat_ctx_request[key] + ' ✖ ' + flat_conf_request[key]);
+            if (options.debug) ctx.throw(400, key + ' : ' + flat_ctx_request[key] + ' ✖ ' + flat_conf_request[key]);
           }
         }
       });
     });
 
-    if (this.status === 404) {
-      yield* next;
+    yield* next;
 
-      // response
-      matchArr.forEach(function (rule) {
-        var flat_conf_response = rule.response || {};
-        var flat_ctx_response = flatten(filterFunc(ctx.response), {safe: true});
+    // response
+    matchArr.forEach(function (rule) {
+      var flat_conf_response = rule.response || {};
+      var flat_ctx_response = flatten(filterFunc(ctx.response), {safe: true});
 
-        Object.keys(flat_conf_response).forEach(function (key) {
-          if (flat_ctx_response[key] === undefined) {
-            debug('%s %s -> %s', ctx.method, ctx.path, key + ' : Not exist!');
-            if (options.debug) return ctx.throw(500, key + ' : Not exist!');
+      Object.keys(flat_conf_response).forEach(function (key) {
+        if (flat_ctx_response[key] === undefined) {
+          debug('%s %s -> %s', ctx.method, ctx.path, key + ' : Not exist!');
+          if (options.debug) ctx.throw(500, key + ' : Not exist!');
+        }
+        if ('function' === typeof flat_conf_response[key]) {
+          if(!flat_conf_response[key].call(ctx, flat_ctx_response[key])) {
+            debug('%s %s -> %s : %s ✖ [Function: %s]', ctx.method, ctx.path, key, flat_ctx_response[key], (flat_conf_response[key].name || 'function'));
+            if (options.debug) ctx.throw(500, key + ' : ' + flat_ctx_response[key] + ' ✖ [Function: ' + (flat_conf_response[key].name || 'function') + ']');
           }
-          if ('function' === typeof flat_conf_response[key]) {
-            if(!flat_conf_response[key].call(ctx, flat_ctx_response[key])) {
-              debug('%s %s -> %s : %s ✖ [Function: %s]', ctx.method, ctx.path, key, flat_ctx_response[key], (flat_conf_response[key].name || 'function'));
-              if (options.debug) return ctx.throw(500, key + ' : ' + flat_ctx_response[key] + ' ✖ [Function: ' + (flat_conf_response[key].name || 'function') + ']');
-            }
-          } else {
-            if (!RegExp(flat_conf_response[key]).test(flat_ctx_response[key])) {
-              debug('%s %s -> %s : %s ✖ %s', ctx.method, ctx.path, key, flat_ctx_response[key], flat_conf_response[key]);
-              if (options.debug) return ctx.throw(500, key + ' : ' + flat_ctx_response[key] + ' ✖ ' + flat_conf_response[key]);
-            }
+        } else {
+          if (!RegExp(flat_conf_response[key]).test(flat_ctx_response[key])) {
+            debug('%s %s -> %s : %s ✖ %s', ctx.method, ctx.path, key, flat_ctx_response[key], flat_conf_response[key]);
+            if (options.debug) ctx.throw(500, key + ' : ' + flat_ctx_response[key] + ' ✖ ' + flat_conf_response[key]);
           }
-        });
+        }
       });
-    }
-  }
+    });
+  };
 }
 
 /**
